6513117751a87d9cba28942b6bf4d21a77862ff3
Jezv95 <138320134+Jezv95@users.noreply.github.com>
2025-06-19 01:28:25 +0000

    arreglado la interaccion js y agregado estilo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+ function getRandomElement(arr){
+  return  arr[Math.floor((Math.random()*arr.length))];
+
+ }
+
+ function finalexcuse(){
+  let randomwho = getRandomElement(who)
+   let randomact = getRandomElement(action)
+    let randomwhat = getRandomElement(what)
+     let randomwhen = getRandomElement(when)
+
+     return `${randomwho} ${randomact} ${randomwhat} ${randomwhen}`
+
+ }
+ 
+
+ document.getElementById("excuse").innerHTML = finalexcuse()
+
+}
+
+
+
+
+
+
