7451458c286334179dfa26924f81292c471c9e09
Jezv95 <138320134+Jezv95@users.noreply.github.com>
2025-06-24 01:19:52 +0000

    refactorizacion y buenas practicas aplicadas

--- src/app.js
+++ src/app.js
@@ -7,32 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function () {
   //write your code here
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising',
+                'during my lunch', 'while I was praying'];
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  function getRandomElement(arr){
+      return  arr[Math.floor((Math.random()*arr.length))];}
 
- function getRandomElement(arr){
-  return  arr[Math.floor((Math.random()*arr.length))];
-
- }
-
- function finalexcuse(){
-  let randomwho = getRandomElement(who)
-   let randomact = getRandomElement(action)
-    let randomwhat = getRandomElement(what)
-     let randomwhen = getRandomElement(when)
-
-     return `${randomwho} ${randomact} ${randomwhat} ${randomwhen}`
-
- }
- 
-
- document.getElementById("excuse").innerHTML = finalexcuse()
-
+  function generateExcuse(){
+      return `${getRandomElement(who)} ${getRandomElement(action)} ${getRandomElement(what)} ${getRandomElement(when)}`}
+      
+  document.getElementById("excuse").innerHTML = generateExcuse()
 }
-
+ 
 
 
 
